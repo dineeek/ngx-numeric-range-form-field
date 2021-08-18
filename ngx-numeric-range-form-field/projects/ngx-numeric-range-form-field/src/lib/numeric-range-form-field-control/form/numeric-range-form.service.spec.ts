ffaf51b501fb4374489f8f8758d83bdb415d8317
Dino Klicek <dineeek@gmail.com>
2021-08-18 19:23:39 +0200

    Added tests

--- ngx-numeric-range-form-field/projects/ngx-numeric-range-form-field/src/lib/numeric-range-form-field-control/form/numeric-range-form.service.spec.ts
+++ ngx-numeric-range-form-field/projects/ngx-numeric-range-form-field/src/lib/numeric-range-form-field-control/form/numeric-range-form.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NumericRangeFormService } from './numeric-range-form.service';
 
@@ -6,7 +7,10 @@ describe('NumericRangeFormService', () => {
 	let service: NumericRangeFormService;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			imports: [ReactiveFormsModule],
+			providers: [NumericRangeFormService]
+		});
 		service = TestBed.inject(NumericRangeFormService);
 	});
 
